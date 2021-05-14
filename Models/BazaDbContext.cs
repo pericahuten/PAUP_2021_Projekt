b37e8237e19f66d9e92555c032a953208886cb4e
pericahuten <perica.huten@student.mev.hr>
2021-05-14 16:19:12 +0200

    dodavanje modela, controllera i view-a za korisnike te bazeDbContext za popisKorisnika

---
+++ Models/BazaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Paup_2021_ServisVozila.Models
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class BazaDbContext : DbContext
+    {
+        public DbSet<Korisnik> PopisKorisnika { get; set; }
+    }
+}
