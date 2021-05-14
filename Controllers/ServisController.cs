ebf7930d93d612efa2de9aca46cdf21010b2134b
pericahuten <perica.huten@student.mev.hr>
2021-05-14 14:52:46 +0200

    push za index

---
+++ Controllers/ServisController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paup_2021_ServisVozila.Controllers
+{
+    public class ServisController : Controller
+    {
+        // GET: Servis
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Početna stranica servisa";
+            ViewBag.Servis = "Servis d.o.o";
+            return View();
+        }
+    }
+}
