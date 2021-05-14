cd4eba869e2d67db6c15b9a03cf2c86c452d97e5
David Bister <68154628+davidbister@users.noreply.github.com>
2021-05-14 18:24:29 +0200

    proba10

---
+++ Controllers/AutomobiliController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paup_2021_ServisVozila.Controllers
+{
+    public class AutomobiliController : Controller
+    {
+        // GET: Automobili
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
