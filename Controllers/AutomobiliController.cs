bdb3d628e50d34430d1328fd17dc421c91cd1f47
pericahuten <80347019+pericahuten@users.noreply.github.com>
2021-05-15 17:14:52 +0200

    Revert "sync"

--- Controllers/AutomobiliController.cs
+++ Controllers/AutomobiliController.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Paup_2021_ServisVozila.Models;
 
 namespace Paup_2021_ServisVozila.Controllers
 {
     public class AutomobiliController : Controller
     {
-        BazaDbContext bazaPodataka = new BazaDbContext();
         // GET: Automobili
         public ActionResult Index()
         {
-            var listaAutomobila = bazaPodataka.PopisAutomobila.OrderBy(x => x.Proizvodac).ToList();
-            return View(listaAutomobila);
+            return View();
         }
     }
 }
